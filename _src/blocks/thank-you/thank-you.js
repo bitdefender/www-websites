55bb23a89d80c9ae2797781b3540f8a15893348c
Matei Iordache <matei.iordache21@gmail.com>
2024-07-18 17:29:13 +0300

    Add thank-you component

--- _src/blocks/thank-you/thank-you.js
+++ _src/blocks/thank-you/thank-you.js
@@ -9,28 +9,37 @@ function dispatchShadowDomLoadedEvent() {
 
 export default function decorate(block, options) {
   const {
-    product, conditionText, saveText, MacOS, Windows, Android, IOS, mainProduct,
-    alignContent, height, type, send2datalayer,
+    product,
   } = options ? options.metadata : block.closest('.section').dataset;
 
   if (options) {
     // eslint-disable-next-line no-param-reassign
     block = block.querySelector('.block');
-    let blockParent = block.closest('.section');
+    const blockParent = block.closest('.section');
     blockParent.classList.add('we-container');
-    if (type) blockParent.classList.add(type);
+  } else {
+    // eslint-disable-next-line no-param-reassign
+    const blockParent = block.closest('.section');
+    blockParent.classList.add('we-container');
   }
 
-  let [richTextPicture, richTextQR] = block.children;
-  
-  block.innerHtml = `<div class="content">
-                <div class="content-left">
-                    ${richTextPicture.children[0].outerHTML}
-                    ${richTextQR}
-                </div>
-                <div class="content-right">
-                    ${richTextPicture.children[0].outerHTML}
-                </div>
-            </div>
-        `;
+  const [richTextPicture, richTextQR] = block.children;
+  const [richTextPictureText, richTextPicturePicture] = richTextPicture.children;
+  const [richTextQRText, richTextQRPicture] = richTextQR.children;
+  const html = `
+      <div class="content-left">
+        ${richTextPictureText.innerHTML}
+        <div class="qr-content">
+          ${richTextQRText.innerHTML}
+          <div class="qr-code">
+            ${richTextQRPicture.innerHTML}
+          </div>
+        </div>
+      </div>
+      <div class="content-right">
+        ${richTextPicturePicture.innerHTML}
+      </div>
+  `;
+
+  block.innerHTML = html;
 }
