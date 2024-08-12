6c8f6cfe1c5739a085bfe660edd163ff29e62252
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2024-08-12 16:24:47 +0300

    DEX-19746 | added real time error validation

--- _src/blocks/form/form.js
+++ _src/blocks/form/form.js
@@ -4,15 +4,44 @@ async function fetchData(url) {
   return json.data;
 }
 
+/**
+ * 
+ * @param {String} email 
+ * @returns {Boolean} wether the email passed is valid or not
+ */
+const validateEmail = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email.toLowerCase());
+}
+
+/**
+ * 
+ * @param {HTMLFormElement} form
+ * set the onChange function for all the form inputs 
+ */
 function onChange(form) {
   // Targeting the anchor inside .button-container
   const submitButton = form.querySelector('input[type="submit"]');
   const emailInput = form.querySelector('input[type="email"]');
 
-  const allCheckboxesChecked = [...form.querySelectorAll('input[type="checkbox"]:required')].every((checkbox) => checkbox.checked);
-  const emailPopulated = emailInput.value.trim() !== '';
+  // set the initial values for normal Forms (except DIP)
+  let emailPopulatedCorrectly = emailInput.value.trim() !== '';
 
-  submitButton.disabled = !((allCheckboxesChecked && emailPopulated));
+  // real time field validation for the email field (For DIP only)
+  // NEEDS TO BE EXTENDED TO WORK FOR ALL FIELDS
+  if (emailInput.classList.contains('input-with-error-field')) {
+    const emailInputErrorField = emailInput.parentElement.querySelector('.input-error-field');
+
+    if (!emailPopulatedCorrectly || !validateEmail(emailInput.value)) {
+      emailInputErrorField.classList.remove('global-display-none');
+      return;
+    }
+
+    emailInputErrorField.classList.add('global-display-none');
+  }
+
+  const allCheckboxesChecked = [...form.querySelectorAll('input[type="checkbox"]:required')].every((checkbox) => checkbox.checked);
+  submitButton.disabled = !((allCheckboxesChecked && emailPopulatedCorrectly));
 }
 
 async function handleSubmit(e, handler) {
@@ -23,6 +52,24 @@ async function handleSubmit(e, handler) {
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log('failed to load module for lib-identity-exposed-onsubmit', error);
+  }
+}
+
+/**
+ * 
+ * @param {HTMLInputElement} input the input to be adde inside the div wrapper
+ * @param {HTMLDivElement} newDivParent the div wrapper to hold the input
+ */
+const wrapInputInsideDiv = (input, newDivParent) => {
+  const label = input.nextElementSibling;
+
+  //if the input has a label, also add it inside the wrapper
+  if (label && label.tagName === 'LABEL') {
+    input.before(newDivParent);
+    newDivParent.append(input, label);
+  } else {
+    input.after(newDivParent);
+    newDivParent.append(input);
   }
 }
 
@@ -73,17 +120,24 @@ export async function createForm(formURL) {
     }
 
     if (field.Type === 'checkbox') {
-      const div = document.createElement('div');
-      div.classList.add('checkbox');
+      const newDivWrapper = document.createElement('div');
+      newDivWrapper.classList.add('checkbox');
 
-      const label = input.nextElementSibling;
-      if (label && label.tagName === 'LABEL') {
-        input.before(div);
-        div.append(input, label);
-      } else {
-        input.after(div);
-        div.append(input);
-      }
+      wrapInputInsideDiv(input, newDivWrapper);
+    }
+
+    if (field.Error) {
+      const newDivWrapper = document.createElement('div');
+      newDivWrapper.classList.add('input-container-with-error');
+
+      //add the error field
+      const errorField = document.createElement('p');
+      errorField.classList.add('input-error-field', 'global-display-none');
+      errorField.textContent = field.Error;
+      newDivWrapper.appendChild(errorField);
+      input.classList.add('input-with-error-field');
+
+      wrapInputInsideDiv(input, newDivWrapper);
     }
   });
   return form;
