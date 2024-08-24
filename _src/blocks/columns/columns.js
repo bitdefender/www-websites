6bd003d5768dae7b99eb02933208b96028d5020b
ltiseanu <ltiseanu@bitdefender.com>
2024-08-24 16:16:50 +0300

    add class

--- _src/blocks/columns/columns.js
+++ _src/blocks/columns/columns.js
@@ -247,7 +247,7 @@ export default function decorate(block, options) {
 
   console.log|('colsadfasfdsadsfdsafdfadsdas ');
   if (type && type === 'video_left') {
-    block.classList(type);
+    block.classList.add(type);
     cols[0].inneHTML = `<video data-type="dam" data-video="" src="${cols[0].innerText}" disableremoteplayback="" playsinline="" controls="" poster="https://www.bitdefender.com/content/dam/bitdefender/solutions/small-business-security/EPP---proteted-v2.jpg"></video>`;
   }
 }
