d3d70b18937eea90496b73f1711fcaf3d128a7a4
Matei Iordache <matei.iordache21@gmail.com>
2024-09-06 18:11:51 +0300

    take 3

--- _src/blocks/new-prod-boxes/new-prod-boxes.js
+++ _src/blocks/new-prod-boxes/new-prod-boxes.js
@@ -621,7 +621,7 @@ export default async function decorate(block, options) {
   if (isInLandingPages) {
     const { decorateIcons, GLOBAL_EVENTS } = await import('../../scripts/utils/utils.js');
     // eslint-disable-next-line import/no-unresolved
-    const { sendAnalyticsPageLoadedEvent } = await import(`${window.location.hostname}/_src-lp/scripts/adobeDataLayer.js`);
+    const { sendAnalyticsPageLoadedEvent } = await import('../_src-lp/scripts/adobeDataLayer.js');
     decorateIcons(block.closest('.section'));
 
     document.addEventListener(GLOBAL_EVENTS.ADOBE_MC_LOADED, () => {
