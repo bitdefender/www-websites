1eee86e28b6c2dbd184238f9be1cd634c5c4f111
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2024-10-04 12:12:39 +0300

    removed analytics call

--- _src/scripts/scripts.js
+++ _src/scripts/scripts.js
@@ -745,6 +745,7 @@ async function loadLazy(doc) {
     loadHeader(doc.querySelector('header'));
   }
 
+  // loadTrackers();
   await loadBlocks(main);
 
   const { hash } = window.location;
@@ -764,8 +765,6 @@ async function loadLazy(doc) {
   if (hasTemplate) {
     loadCSS(`${window.hlx.codeBasePath}/scripts/template-factories/${templateMetadata}-lazy.css`);
   }
-
-  loadTrackers();
 
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
