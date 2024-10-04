ce653b69dd608d8e8ae99d13f5512e60184f33aa
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2024-10-04 13:46:34 +0300

    tried to improve loadEager logic

--- _src/scripts/lib-franklin.js
+++ _src/scripts/lib-franklin.js
@@ -752,19 +752,27 @@ export function decorateButtons(element) {
 export async function waitForLCP(lcpBlocks) {
   const block = document.querySelector('.block');
   const hasLCPBlock = (block && lcpBlocks.includes(block.dataset.blockName));
-  if (hasLCPBlock) await loadBlock(block);
-
-  document.body.style.display = null;
-  const lcpCandidate = document.querySelector('main img');
-  await new Promise((resolve) => {
-    if (lcpCandidate && !lcpCandidate.complete) {
-      lcpCandidate.setAttribute('loading', 'eager');
-      lcpCandidate.addEventListener('load', resolve);
-      lcpCandidate.addEventListener('error', resolve);
-    } else {
-      resolve();
+  if (hasLCPBlock) {
+    document.body.style.display = null;
+    for (const lcpBlockName of lcpBlocks) {
+      const lcpBlock = document.querySelector(`.${lcpBlockName}`);
+      if (!lcpBlock) {
+        continue;
+      }
+      await loadBlock(lcpBlock);
     }
-  });
+
+    const lcpCandidate = document.querySelector('main img');
+    await new Promise((resolve) => {
+      if (lcpCandidate && !lcpCandidate.complete) {
+        lcpCandidate.setAttribute('loading', 'eager');
+        lcpCandidate.addEventListener('load', resolve);
+        lcpCandidate.addEventListener('error', resolve);
+      } else {
+        resolve();
+      }
+    });
+  }
 }
 
 /**
