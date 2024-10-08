7243595444660a594f9a896e9de6fe0565a1e773
ltiseanu <ltiseanu@bitdefender.com>
2024-10-08 17:58:42 +0300

    update

--- _src/scripts/utils/utils.js
+++ _src/scripts/utils/utils.js
@@ -848,7 +848,7 @@ export function pushTrialDownloadToDataLayer() {
     || getMetadata('og:title')
   );
 
-  if (TRACKED_PRODUCTS_COMPARISON[0].productCode === 'av') {
+  if (TRACKED_PRODUCTS_COMPARISON && TRACKED_PRODUCTS_COMPARISON[0].productCode === 'av') {
     getTrialID = '8430';
   }
 
