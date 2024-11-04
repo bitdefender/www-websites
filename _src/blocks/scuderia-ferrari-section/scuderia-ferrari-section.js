88fde721218531f4bf0f19831dfcc7fe31ad11f8
Alexandru-Butuza <abutuza@bitdefender.com>
2024-11-04 16:00:33 +0200

    implemented design feeback for scuderia ferrari page

--- _src/blocks/scuderia-ferrari-section/scuderia-ferrari-section.js
+++ _src/blocks/scuderia-ferrari-section/scuderia-ferrari-section.js
@@ -1,25 +1,17 @@
 function replacePlaceholderWithVideo(videoUrl) {
-  // Find the div that contains the {video} text
   const placeholderDiv = [...document.querySelectorAll('div')].find((div) => div.textContent.trim() === '{video}');
-
   if (!placeholderDiv) {
     return;
   }
-  // Create a new video container
   const videoContainer = document.createElement('div');
   videoContainer.className = 'video-container';
 
-  // Create an iframe element for embedding YouTube video
   const iframeElement = document.createElement('iframe');
   iframeElement.setAttribute('src', videoUrl);
   iframeElement.setAttribute('frameborder', '0');
   iframeElement.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
   iframeElement.setAttribute('allowfullscreen', '');
-
-  // Add the iframe to the container
   videoContainer.appendChild(iframeElement);
-
-  // Replace the placeholder div with the video container
   placeholderDiv.replaceWith(videoContainer);
 }
 
