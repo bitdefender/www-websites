0244e93a5a9a7b36333949a02d3849f265a7bf85
Matei Iordache <matei.iordache21@gmail.com>
2024-11-05 15:44:24 +0200

    fix: streamline mbox-canvas rendering by removing unnecessary console logs and updating innerHTML assignment

--- _src/blocks/mbox-canvas/mbox-canvas.js
+++ _src/blocks/mbox-canvas/mbox-canvas.js
@@ -9,18 +9,12 @@ export default async function decorate(block) {
         </div>
     `;
   const offer = await Target.getMbox('something');
-  console.log(offer)
-  console.log(offer.content.offer)
   const page = await fetch(`http://localhost:3000/en-us/webview/webview-table.plain.html`);
   const aemHeaderHtml = await page.text();
-  console.log(offer)
-  console.log(aemHeaderHtml);
   let newHtml = document.createElement('div');
   newHtml.innerHTML = aemHeaderHtml;
   decorateMain(newHtml);
   detectModalButtons(newHtml);
-  await loadBlocks(newHtml);
-  // console.log(aemHeaderHtml);
-  console.log(newHtml);
-  block.querySelector('[data-mbox=something]').innerHTML = newHtml.outerHTML;
+  block.querySelector('[data-mbox=something]').innerHTML = newHtml.innerHTML;
+  await loadBlocks(block.querySelector('[data-mbox=something]'));
 }
