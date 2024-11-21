fa44cb3a41c413e7e28ef27c4cadc5fbf927aa88
Matei Iordache <matei.iordache21@gmail.com>
2024-11-21 17:50:47 +0200

    add initial fragment styles and improve error logging in loadFragment function

--- _src/blocks/fragment/fragment.js
+++ _src/blocks/fragment/fragment.js
@@ -24,6 +24,7 @@ async function loadFragment(path) {
       // eslint-disable-next-line no-param-reassign
       path = path.replace(/lang/g, `${language}-${country}`);
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.log(error);
     }
     const resp = await fetch(`${path}.plain.html`);
