cad843816b5dcb2bb8eb2e45172f124536e04cf7
Matei Iordache <matei.iordache21@gmail.com>
2024-11-27 16:01:52 +0200

    add new SVG icons and update link-checker styles

--- _src/blocks/link-checker/link-checker.js
+++ _src/blocks/link-checker/link-checker.js
@@ -1,21 +1,25 @@
 class StatusMessageFactory {
   static createMessage(status, url) {
+    console.log(status);
     const messages = {
-      safe: { text: 'The link is safe with no signs of harmful activity. You can go ahead and keep staying cautious online.', className: 'result safe' },
-      so_far_so_good_1: { text: 'We haven\'t seen any suspicious activity from this link.', className: 'result safe' },
-      so_far_so_good_2: { text: `This link looks safe, but the domain '${url.hostname}' has been connected to harmful links in the past. To stay protected, check any other links from this domain using our tool and keep your security software updated.`, className: 'result safe' },
-      phishing: { text: 'This link leads to a phishing site designed to steal personal information like passwords or financial data. Stay away from the site and ensure your security software is active.', className: 'result danger' },
-      malware: { text: 'This link is known to distribute malware. Accessing it may harm your device, steal your data, or allow unauthorized access. Stay away from the site and ensure your security software is active.', className: 'result danger' },
-      untrusted: { text: 'This link appears suspicious and may not be trustworthy. It’s best to avoid accessing it. Keep your security software active and steer clear of the site.', className: 'result warning' },
-      fraud: { text: 'This link directs to a fraudulent site intended to trick users and steal sensitive data. Stay away from the site and ensure your security software is active.', className: 'result danger' },
-      spam: { text: 'This link has been identified in spam emails, which often contain malicious content. Avoid clicking on it, as it may lead to harmful sites or scams. Ensure your security measures are in place.', className: 'result warning' },
-      pua: { text: 'This link is associated with apps that could slow down your device or compromise your privacy. It’s best to avoid the site and make sure your security settings are active.', className: 'result danger' },
-      miner: { text: 'This URL is linked to cryptocurrency mining activities, which may use your device\'s resources without your consent. Avoid visiting the site and ensure your security protections are in place.', className: 'result danger' },
-      'malware-hd': { text: 'This URL is likely to contain malware, posing a significant threat. It\'s strongly advised to avoid accessing it and ensure your security protections are active and up to date.', className: 'result danger' },
-      homograph: { text: 'This link is designed to look like a trusted site using tricky characters. Don’t click the link and make sure your security software is updated to protect your device.', className: 'result danger' },
-      'c&c': { text: 'This URL is linked to a server used to command and control malware on infected devices. Don’t click the link and make sure your security software is up to date to keep your device safe.', className: 'result danger' },
-      'miner-server': { text: 'This URL is linked to crypto mining activity, which could use your device\'s resources if accessed. It’s recommended not to click the link and ensure your security software is up to date.', className: 'result danger' },
-      malvertising: { text: 'This link is connected to harmful ads that could affect your device and expose your personal data, such as your passwords, credit card information, email addresses, or browsing history. Avoid clicking on it and keep your security software updated to stay safe.', className: 'result danger' },
+      1: { text: 'The link is safe with no signs of harmful activity. You can go ahead and keep staying cautious online.', className: 'result safe' },
+      2: { text: 'We haven\'t seen any suspicious activity from this link.', className: 'result safe' },
+      3: { text: `This link looks safe, but the domain '${url.hostname}' has been connected to harmful links in the past. To stay protected, check any other links from this domain using our tool and keep your security software updated.`, className: 'result safe' },
+      5: { text: 'This link is known to distribute malware. Accessing it may harm your device, steal your data, or allow unauthorized access. Stay away from the site and ensure your security software is active.', className: 'result danger' },
+      17: { text: 'This link appears suspicious and may not be trustworthy. It’s best to avoid accessing it. Keep your security software active and steer clear of the site.', className: 'result danger' },
+      8: { text: 'This link directs to a fraudulent site intended to trick users and steal sensitive data. Stay away from the site and ensure your security software is active.', className: 'result danger' },
+      15: { text: 'This link has been identified in spam emails, which often contain malicious content. Avoid clicking on it, as it may lead to harmful sites or scams. Ensure your security measures are in place.', className: 'result danger' },
+      11: { text: 'This link is associated with apps that could slow down your device or compromise your privacy. It’s best to avoid the site and make sure your security settings are active.', className: 'result danger' },
+      13: { text: 'This URL is linked to cryptocurrency mining activities, which may use your device\'s resources without your consent. Avoid visiting the site and ensure your security protections are in place.', className: 'result danger' },
+      16: { text: 'This URL is likely to contain malware, posing a significant threat. It\'s strongly advised to avoid accessing it and ensure your security protections are active and up to date.', className: 'result danger' },
+      12: { text: 'This link is designed to look like a trusted site using tricky characters. Don’t click the link and make sure your security software is updated to protect your device.', className: 'result danger' },
+      6: { text: 'This URL is linked to a server used to command and control malware on infected devices. Don’t click the link and make sure your security software is up to date to keep your device safe.', className: 'result danger' },
+      14: { text: 'This URL is linked to crypto mining activity, which could use your device\'s resources if accessed. It’s recommended not to click the link and ensure your security software is up to date.', className: 'result danger' },
+      10: { text: 'This link is connected to harmful ads that could affect your device and expose your personal data, such as your passwords, credit card information, email addresses, or browsing history. Avoid clicking on it and keep your security software updated to stay safe.', className: 'result danger' },
+      18: { text: 'This link is unsafe and could harm your device or steal your personal information. Avoid clicking on it and keep your security software updated to stay safe.', className: 'result danger' },
+      4: { text: 'This link is dangerous and can compromise your personal information or harm your device. Do not click it, and ensure your security software is up-to-date to stay protected from threats.', className: 'result danger' },
+      7: { text: 'This link is a threat, exposing you to malicious ads and phishing attempts that can steal your information and damage your device. Do not interact with it, and ensure your security software is updated.', className: 'result danger' },
+      9: { text: 'This link leads to a phishing site designed to steal personal information like passwords or financial data. Stay away from the site and ensure your security software is active.', className: 'result danger' },
       other: { text: 'This link is unsafe and could harm your device or steal your personal information. Avoid clicking on it and keep your security software updated to stay safe.', className: 'result danger' },
     };
     return messages[status] || { text: `Status: ${status}`, className: 'result warning' };
@@ -52,11 +56,26 @@ async function checkLink(input, result) {
   }
 
   const data = await response.json();
-
-  const status = data.status[0];
+  console.log(data);
+  const { status } = data;
   const message = StatusMessageFactory.createMessage(status, url);
   result.textContent = message.text;
   result.className = message.className;
+}
+
+function copyToClipboard(block) {
+  // Get the text field
+  const copyText = document.getElementById('link-checker-input');
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert(`Copied the text: ${copyText.value}`);
 }
 
 export default function decorate(block) {
@@ -67,16 +86,35 @@ export default function decorate(block) {
   const input = document.createElement('input');
   input.type = 'text';
   input.placeholder = 'Enter URL to check';
+  input.id = 'link-checker-input';
   inputContainer.appendChild(input);
 
   const button = document.createElement('button');
   button.textContent = 'Check URL';
+  button.classList.add('check-url');
   inputContainer.appendChild(button);
 
   const result = document.createElement('div');
   result.textContent = 'Add your link or paste it from the device’s clipboard.';
   result.className = 'result';
   block.appendChild(result);
+
+  const buttonsContainer = document.createElement('div');
+  buttonsContainer.classList.add('buttons-container');
+
+  const shareButton = document.createElement('button');
+  shareButton.innerHTML = '<span>Share Result</span>';
+  shareButton.classList.add('share-button');
+  shareButton.addEventListener('click', copyToClipboard(block));
+
+  const checkAnother = document.createElement('button');
+  checkAnother.innerHTML = '<span>Check Another Link</span>';
+  checkAnother.classList.add('check-another-button');
+
+  buttonsContainer.appendChild(shareButton);
+  buttonsContainer.appendChild(checkAnother);
+
+  block.appendChild(buttonsContainer);
 
   button.addEventListener('click', () => checkLink(input, result));
 }
