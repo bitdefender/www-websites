a1a48be2063ac6a7ccaf299beb4c2b6700f3a74b
ltiseanu <ltiseanu@bitdefender.com>
2024-12-09 21:43:48 +0200

    removing unused const

--- _src/scripts/libs/store/store.js
+++ _src/scripts/libs/store/store.js
@@ -796,57 +796,8 @@ class BitCheckout {
 		}
 	}
 
-
-	static monthlyProducts2 = ["psm", "pspm", "vpn-monthly", "passm", "pass_spm", "secpassm", "dipm", "us_i_m",
-		"us_f_m", "us_pf_m", "us_pi_m", "us_pie_m", "us_pfe_m"]
-
 	// this products come with device_no set differently from the init-selector api where they are set to 1
 	static wrongDeviceNumber = ["bms", "mobile", "ios", "mobileios", "psm", "passm"]
-
-	static productId2 = {
-		av: "com.bitdefender.cl.av",
-		is: "com.bitdefender.cl.is",
-		tsmd: "com.bitdefender.cl.tsmd",
-		fp: "com.bitdefender.fp",
-		ps: "com.bitdefender.premiumsecurity",
-		psm: "com.bitdefender.premiumsecurity",
-		psp: "com.bitdefender.premiumsecurityplus",
-		pspm: "com.bitdefender.premiumsecurityplus",
-		soho: "com.bitdefender.soho",
-		mac: "com.bitdefender.avformac",
-		vpn: "com.bitdefender.vpn",
-		"vpn-monthly": "com.bitdefender.vpn",
-		pass: "com.bitdefender.passwordmanager",
-		passm: "com.bitdefender.passwordmanager",
-		pass_sp: "com.bitdefender.passwordmanager",
-		pass_spm: "com.bitdefender.passwordmanager",
-		secpass: 'com.bitdefender.securepass',
-    	secpassm: 'com.bitdefender.securepass',
-		bms: "com.bitdefender.bms",
-		mobile: "com.bitdefender.bms",
-		ios: "com.bitdefender.iosprotection",
-		mobileios: "com.bitdefender.iosprotection",
-		dip: "com.bitdefender.dataprivacy",
-		dipm: "com.bitdefender.dataprivacy",
-		avpm: 'com.bitdefender.cl.avplus.v2',
-		// DLP
-		ts_i: 'com.bitdefender.tsmd.v2',
-		ts_f: 'com.bitdefender.tsmd.v2',
-		ps_i: 'com.bitdefender.premiumsecurity.v2',
-		ps_f: 'com.bitdefender.premiumsecurity.v2',
-		us_i: 'com.bitdefender.ultimatesecurityeu.v2',
-		us_i_m: 'com.bitdefender.ultimatesecurityeu.v2',
-		us_f: 'com.bitdefender.ultimatesecurityeu.v2',
-		us_f_m: 'com.bitdefender.ultimatesecurityeu.v2',
-		us_pf: 'com.bitdefender.ultimatesecurityeu.v2',
-		us_pf_m: 'com.bitdefender.ultimatesecurityeu.v2',
-		us_pi: 'com.bitdefender.ultimatesecurityplusus.v2',
-		us_pi_m: 'com.bitdefender.ultimatesecurityplusus.v2',
-		us_pie: 'com.bitdefender.ultimatesecurityplusus.v2',
-		us_pie_m: 'com.bitdefender.ultimatesecurityplusus.v2',
-		us_pfe: 'com.bitdefender.ultimatesecurityplusus.v2',
-		us_pfe_m: 'com.bitdefender.ultimatesecurityplusus.v2',
-	}
 
 	static names = {
 		pass: "Bitdefender Password Manager",
