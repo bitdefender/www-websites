50fe93701cffaf89d8faa7832aece9a3c8cf6735
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2025-03-10 11:44:09 +0200

    DEX-21585 | applyPropositions attempt

--- _src/scripts/libs/data-layer.js
+++ _src/scripts/libs/data-layer.js
@@ -573,6 +573,36 @@ export class Target {
     }
 
     this.getOffers(['initSelector-mbox', 'buyLinks-mbox', 'geoip-flag-mbox', 'taget-global-mbox']);
+
+    window.adobeDataLayer.push({
+      mboxes: ['initSelector-mbox', 'buyLinks-mbox', 'geoip-flag-mbox', 'taget-global-mbox']
+    });
+
+    document.addEventListener('alloy-finished-loading', () => {
+      if (window.propositions.length) {
+        window.alloy('applyPropositions', {
+          "propositions": window.propositions,
+          "metadata": {
+            "initSelector-mbox": {
+              "selector": "#initSelector",
+              "actionType": "setHtml"
+            },
+            "buyLinks-mbox": {
+              "selector": "#buyLinks",
+              "actionType": "setHtml"
+            },
+            "geoip-flag-mbox": {
+              "selector": "#geoIp",
+              "actionType": "setHtml"
+            },
+            "taget-global-mbox": {
+              "selector": "#targetGlobal",
+              "actionType": "setHtml"
+            }
+          }
+        })
+      }
+    });
   };
 
   /**
@@ -666,40 +696,47 @@ export class Target {
 
     const notRequestedMboxes = mboxes.filter(mbox => !this.#cachedMboxes.has(`${mbox}_${JSON.stringify(parameters)}`));
     if (notRequestedMboxes.length) {
-      const notRequestedOffersCall = window.alloyProxy('sendEvent', {
-        decisionScopes: notRequestedMboxes,
-        data: {
-          "__adobe": {
-            "target": Object.assign({}, this.#urlParameters,
-              parameters ? parameters : {},
-              profileParameters ? profileParameters : {}
-            ),
-          }
-        },
-        renderDecisions: true
-      });
-
-      notRequestedOffersCall.then(result => {
-        window.alloyProxy("sendEvent", {
-          "xdm": {
-            "eventType": "decisioning.propositionDisplay",
-            "_experience": {
-              "decisioning": {
-                "propositions": result.propositions
-              }
-            }
-          }
-        });
-      });
+      // const notRequestedOffersCall = window.alloyProxy('sendEvent', {
+      //   decisionScopes: notRequestedMboxes,
+      //   data: {
+      //     "__adobe": {
+      //       "target": Object.assign({}, this.#urlParameters,
+      //         parameters ? parameters : {},
+      //         profileParameters ? profileParameters : {}
+      //       ),
+      //     }
+      //   },
+      //   renderDecisions: true
+      // });
 
       notRequestedMboxes.forEach(mbox => {
         const receivedMboxOfferCall = new Promise((resolve, reject) => {
-          notRequestedOffersCall.then(result => {
-            const mboxResult = result.propositions.find(offer => offer.scope === mbox)?.items[0].data?.content;
-            resolve(mboxResult);
-          }).catch(e => {
-            reject(e);
-          });
+
+          if (window.propositions) {
+            const mboxResult = window.propositions.find(offer => offer.scope === mbox)?.items[0].data?.content;
+            if (mboxResult) {
+              resolve(mboxResult);
+            } else {
+              reject();
+            }
+          } else {
+
+            document.addEventListener('alloy-finished-loading', () => {
+              const mboxResult = window.propositions.find(offer => offer.scope === mbox)?.items[0].data?.content;
+              if (mboxResult) {
+                resolve(mboxResult);
+              } else {
+                reject();
+              }
+            });
+          }
+
+          // notRequestedOffersCall.then(result => {
+          //   const mboxResult = result.propositions.find(offer => offer.scope === mbox)?.items[0].data?.content;
+          //   resolve(mboxResult);
+          // }).catch(e => {
+          //   reject(e);
+          // });
         });
 
         this.#cachedMboxes.set(`${mbox}_${JSON.stringify(parameters)}`, receivedMboxOfferCall);
@@ -713,6 +750,10 @@ export class Target {
       acc[mbox] = resolvedMboxes[index].value || null;
       return acc;
     }, {});
+
+    if (mboxes[0] === 'webview') {
+      return {};
+    }
 
     return mboxes.length > 1 ? offersResult : offersResult[mboxes[0]];
   }
