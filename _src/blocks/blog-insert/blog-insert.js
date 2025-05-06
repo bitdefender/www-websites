3312659ed2d6692ef2f4ddf0d7c2513ffd863e82
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2025-05-06 17:19:16 +0300

    DEX-22502 | fix: added alt attribute to blog-insert

--- _src/blocks/blog-insert/blog-insert.js
+++ _src/blocks/blog-insert/blog-insert.js
@@ -1,9 +1,15 @@
 export default function decorate(block) {
+  const metadata = block.closest('.section').dataset;
+
   const urlDiv = block.querySelector('div > div:nth-child(2) > div');
   const url = urlDiv.textContent.trim();
 
   // Get the picture element
   const pictureElement = block.querySelector('picture');
+  const imageElement = pictureElement.querySelector('img');
+  if (imageElement && metadata.alt) {
+    imageElement.alt = metadata.alt;
+  }
 
   // Create a new anchor element
   const anchorElement = document.createElement('a');
