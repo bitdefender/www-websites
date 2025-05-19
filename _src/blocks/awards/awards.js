1f53346460cae422fc99800d3958e93b93b1306d
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2025-05-19 13:44:43 +0300

    DEX-22918 | test for husky

--- _src/blocks/awards/awards.js
+++ _src/blocks/awards/awards.js
@@ -1,2 +1,2 @@
-import{matchHeights as a}from"../../scripts/utils/utils.js";function s(c){const e=r=>{const{children:t}=r;return t.length};[...c.children].forEach(r=>{e(r)===2&&(r.classList.add("cards"),a(r,"p"),a(r,"h3"),[...r.children].forEach(t=>{t.classList.add("card")}))})}export{s as default};
+import{matchHeights as a}from"../../scripts/utils/utils.js";function n(o){const c=r=>{const{children:t}=r;return t.length};console.log("a"),[...o.children].forEach(r=>{c(r)===2&&(r.classList.add("cards"),a(r,"p"),a(r,"h3"),[...r.children].forEach(t=>{t.classList.add("card")}))})}export{n as default};
 //# sourceMappingURL=awards.js.map
