0216687f84033bad2cc5e8675674cd3cc6c9ae93
ltiseanu <ltiseanu@bitdefender.com>
2025-08-04 18:13:59 +0300

    fix lint

--- _src/blocks/scam-masters/scam-masters.js
+++ _src/blocks/scam-masters/scam-masters.js
@@ -114,7 +114,7 @@ function processSpecialParagraphs(question, index) {
         if (!question.querySelector('p.tries')) {
           const triesParagraph = document.createElement('p');
           triesParagraph.classList.add('tries');
-          triesParagraph.setAttribute('data-singletry', singleTry.trim())
+          triesParagraph.setAttribute('data-singletry', singleTry.trim());
           triesParagraph.innerHTML = manyTries.trim().replace('x', `<span>${triesCount}</span>`);
 
           const scamButton = question.querySelector('a[href="#not-a-scam"]');
