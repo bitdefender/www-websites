f1904dcd814e6363c7511d4d551a2b9bbfd343cd
ltiseanu <ltiseanu@bitdefender.com>
2025-08-20 11:31:17 +0300

    update

--- _src/blocks/scam-masters/scam-masters.js
+++ _src/blocks/scam-masters/scam-masters.js
@@ -115,7 +115,7 @@ function processSpecialParagraphs(question, index) {
           const triesParagraph = document.createElement('p');
           triesParagraph.classList.add('tries');
           triesParagraph.setAttribute('data-singletry', singleTry.trim());
-          triesParagraph.innerHTML = manyTries.trim().replace('x', `<span>${triesCount}</span>`);
+          triesParagraph.innerHTML = manyTries?.trim().replace('x', `<span>${triesCount}</span>`);
 
           const scamButton = question.querySelector('a[href="#not-a-scam"]');
           if (scamButton && scamButton.parentNode) {
