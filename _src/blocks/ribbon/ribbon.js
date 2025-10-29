ad650e1f7ddbcb4450d77c7ea90234e2bc375c15
ltiseanu <ltiseanu@bitdefender.com>
2025-10-29 14:53:42 +0200

    DEX-25172 - BF 2025 - Website/Solutions Page - Landing Page - DEV

--- _src/blocks/ribbon/ribbon.js
+++ _src/blocks/ribbon/ribbon.js
@@ -1,5 +1,6 @@
 export default function decorate(block) {
   const [, backgroundEl] = block.children;
+  block.innerHTML = block.innerText.replace(/0\s*%/g, `<span data-store-text-variable>{GLOBAL_BIGGEST_DISCOUNT_PERCENTAGE}</span>`);
 
   if (backgroundEl) {
     const backgroundImgEl = backgroundEl.querySelector('img');
