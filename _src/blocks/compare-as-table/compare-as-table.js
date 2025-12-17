8478e8b3ea07b091c772ab9932e1d39fac955b78
ltiseanu <ltiseanu@bitdefender.com>
2025-12-17 19:03:38 +0200

    DEX-25832 - Updated Header for AV Free Page - Websites

--- _src/blocks/compare-as-table/compare-as-table.js
+++ _src/blocks/compare-as-table/compare-as-table.js
@@ -16,13 +16,13 @@ function setDiscountedPriceAttribute(type, hideDecimals, prodName) {
   return priceAttribute;
 }
 
-async function updateProductPrice(prodName, saveText = null, buyLinkSelector = null, billed = null, type = null, hideDecimals = null, perPrice = '') {
-  console.log('buyLinkSelector', buyLinkSelector)
+async function updateProductPrice(prodName, saveText = null, buyLinkSelector = null, billed = null, type = null, hideDecimals = null, perPrice = '', key = null) {
   let priceElement = document.createElement('div');
   let newPrice = document.createElement('span');
   const customLink = buyLinkSelector.querySelector('a')?.getAttribute('href');
   let priceAttribute = setDiscountedPriceAttribute(type, hideDecimals, prodName);
-  
+  const savePriceEl = saveText ? saveText.replace('0%', '<span data-store-discount="percentage"></span>'): '';
+  const billedText = billed?.innerHTML || '';
   newPrice.setAttribute('data-store-price', priceAttribute);
 
   let oldPrice = 'data-store-price="full"';
@@ -33,23 +33,25 @@ async function updateProductPrice(prodName, saveText = null, buyLinkSelector = n
   }
 
   priceElement.innerHTML = `
-      <div class="hero-aem__price mt-3">
-        <div class="oldprice-container">
-          <span class="prod-oldprice" ${oldPrice} data-store-hide="no-price=discounted"></span>
-          <span class="prod-save" data-store-hide="no-price=discounted">${saveText} <span data-store-discount="percentage"></span> </span>
-        </div>
-        <div class="newprice-container mt-2">
-          <span class="prod-newprice"> ${newPrice.outerHTML}  ${perPrice && `<sup class="per-m">${perPrice.textContent.replace('0', '')}</sup>`}</span>
-        </div>
-        ${billed ? `<div class="billed">${billed.innerHTML.replace('0', `<span class="newprice-2" ${billedPrice}></span>`)}</div>` : ''}
-        <a ${customLink ? `href="${customLink}"` : 'href="#" data-store-buy-link'} class="button primary no-arrow">${buyLinkSelector?.innerText}</a>
-      </div>`;
+    <div class="hero-aem__price mt-3">
+      <div class="oldprice-container">
+        <span class="prod-oldprice" ${oldPrice} data-store-hide="no-price=discounted"></span>
+      </div>
+      <div class="newprice-container mt-2">
+        <span class="prod-newprice"> ${newPrice.outerHTML}  ${perPrice && `<sup class="per-m">${perPrice.textContent.replace('0', '')}</sup>`}</span>
+        <span class="prod-save green-pill" data-store-hide="no-price=discounted">${savePriceEl}</span>
+      </div>
+      
+      ${billedText.includes('0') ? `<div class="billed"> ${billedText.replace('0', `<span class="newprice-2" ${billedPrice}></span>`)}</div>` : ''}
+      ${!billedText.includes('0') ? `<div class="billed">${billedText}</div>` : ''}
+
+      <a ${customLink ? `href="${customLink}"` : 'href="#"'} href="#" data-store-buy-link class="button ${key === 0 ? 'primary' : ''} no-arrow">${buyLinkSelector?.innerText}</a>
+    </div>`;
   return priceElement;
 }
 
 export default async function decorate(block) {
-  const blockParent = block.closest('.section');
-  const { products } = block.closest('.section').dataset;
+  const { products, savetext } = block.closest('.section').dataset;
   if (!products) return;
 
   const tables = block.querySelectorAll('table');
@@ -57,8 +59,10 @@ export default async function decorate(block) {
 
   for (const [key, table] of [...tables].entries()) {
     const rows = table.querySelectorAll('tr');
+    const billed = rows[2];
     const priceRow = rows[3];
     if (!priceRow) continue;
+
     const productStr = productList[key];
     if (!productStr) continue;
 
@@ -71,13 +75,19 @@ export default async function decorate(block) {
     table.setAttribute('data-store-department', 'consumer');
     table.querySelector('a[href*="#buylink"]')?.setAttribute('data-store-buy-link', '');
 
-    const priceBox = await updateProductPrice(name, '', priceRow);
-    //priceRow.innerHTML = priceBox.innerHTML;
+    const priceBox = await updateProductPrice(name, savetext, priceRow, billed, '', '' , '', key);
+    priceRow.innerHTML = priceBox.innerHTML;
+
+    if (billed.innerText) billed.innerText = '';
+
+    // add icon on features:
+    const allfeats = rows[rows.length - 1];
+    allfeats.innerHTML = allfeats.innerHTML.replace(/\[yes\]/g, '<span class="yes-check"></span>').replace(/\[no\]/g, '<span class="no-check"></span>');
 
     matchHeights(block, 'table tr:nth-of-type(1)');
     matchHeights(block, 'table tr:nth-of-type(2)');
     matchHeights(block, 'table tr:nth-of-type(3)');
     matchHeights(block, 'table tr:nth-of-type(4)');
     matchHeights(block, 'table tr:nth-of-type(5)');
-  } 
+  }
 }
