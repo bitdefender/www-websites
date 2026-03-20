6155a7c285ab2cc806e91038984146fefc2a234d
ltiseanu <ltiseanu@bitdefender.com>
2026-03-20 11:10:50 +0200

    fix js

--- _src/blocks/columns/columns.js
+++ _src/blocks/columns/columns.js
@@ -473,7 +473,7 @@ export default function decorate(block) {
     matchHeights(block, 'div > div:not(:first-of-type) ul');
   }
 
-  matchHeights(block, 'div > div > p:first-of-type > strong')
+  matchHeights(block, 'div > div > p:first-of-type > strong');
 
   if (block.classList.contains('text-over-image')) matchHeights(block, '.columns > div > div');
 
