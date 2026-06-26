f2e65eb204787fdb61784a5e2e6e427cd253940f
andserban <andserban@bitdefender.com>
2026-06-26 15:47:36 +0300

    fix for match height

--- _src/scripts/utils/utils.js
+++ _src/scripts/utils/utils.js
@@ -1215,7 +1215,7 @@ export const wrapChildrenWithStoreContext = (element, {
   element.appendChild(context);
 };
 
-const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.56/';
+const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.57/';
 
 export const getDsnBase = () => {
   try {
