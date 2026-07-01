1519016741fcf869a22802ccbcf2c3c873b34d83
andserban <andserban@bitdefender.com>
2026-07-01 11:35:25 +0300

    add padding to features box

--- _src/scripts/utils/utils.js
+++ _src/scripts/utils/utils.js
@@ -1215,7 +1215,7 @@ export const wrapChildrenWithStoreContext = (element, {
   element.appendChild(context);
 };
 
-const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.59/';
+const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.60/';
 
 export const getDsnBase = () => {
   try {
