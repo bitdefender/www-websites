3d693f7d48bd8bfc31f83d21334842db5a285750
abutuza <abutuza@bitdefender.com>
2026-07-03 12:28:10 +0300

    DEX-27861 | fix: mobile style

--- _src/blocks/awards-search/awards-search.js
+++ _src/blocks/awards-search/awards-search.js
@@ -234,6 +234,7 @@ export default async function decorate(block) {
       style.textContent = `
         .bd-accordion-bg-container {
           padding-top: 0 !important;
+          padding-bottom: 0 !important;
         }
       `;
       accordionShadowRoot.appendChild(style);
