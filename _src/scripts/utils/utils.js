a9fcfb832cfa22fc188aaa657d59aeff6ee98860
andserban <andserban@bitdefender.com>
2026-07-13 16:01:01 +0300

    fallback update

--- _src/scripts/utils/utils.js
+++ _src/scripts/utils/utils.js
@@ -1215,7 +1215,7 @@ export const wrapChildrenWithStoreContext = (element, {
   element.appendChild(context);
 };
 
-const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.70/';
+const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.72/';
 
 export const getDsnBase = () => {
   try {
