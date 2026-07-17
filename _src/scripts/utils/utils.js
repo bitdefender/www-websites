94f86e1ca706e6c49567798f440741bc4a7fb4b7
andserban <andserban@bitdefender.com>
2026-07-17 11:38:40 +0300

    update dependencies to latest versions

--- _src/scripts/utils/utils.js
+++ _src/scripts/utils/utils.js
@@ -1215,7 +1215,7 @@ export const wrapChildrenWithStoreContext = (element, {
   element.appendChild(context);
 };
 
-const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.88/';
+const DSN_FALLBACK = 'https://esm.sh/@repobit/dex-system-design@0.23.90/';
 
 export const getDsnBase = () => {
   try {
